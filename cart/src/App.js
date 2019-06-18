61ce12e3dd20ac4179bb9c1dcad34998d6024e6f
Bobby Lei <bobby.lei@hreasily.com>
2019-06-19 09:35:27 +1200

    Implement add product to cart using CustomEvent

--- cart/src/App.js
+++ cart/src/App.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () =>
-  <sidebar>
-    <h2>Cart</h2>
-    <p>Your cart is empty</p>
-  </sidebar>;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('addToCart', (event) => {
+      this.setState({ products: [...this.state.products, event.detail] });
+    }, false);
+  }
+
+  render() {
+    return (
+      <nav>
+        <h2>Cart</h2>
+        {this.renderProducts()}
+      </nav>
+    );
+  }
+
+  renderProducts() {
+    if (this.state.products.length === 0) {
+      return <p>Your cart is empty</p>;
+    }
+    return <ul>{this.state.products.map(this.renderProduct)}</ul>;
+  }
+  renderProduct(product, index) {
+    return <li key={index}>{product.product} - ${product.price}</li>;
+  }
+}
