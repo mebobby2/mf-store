2ad026a139ef5678289da712b86d45d81cf5fd37
Bobby Lei <bobby.lei@hreasily.com>
2019-06-18 22:01:17 +1200

    Progressively stream the html to the browser

--- homepage/server.js
+++ homepage/server.js
@@ -1,29 +1,61 @@
 const express = require('express');
 const server = express();
 const request = require('request');
+const ejs = require('ejs');
+const padRight = require('pad-right');
 
 server.set('view engine', 'ejs');
 
-server.get('/', (req, res) =>
-  Promise.all([
-    getContents('http://localhost:8080'),
-    getContents('http://localhost:8081'),
-    getContents('http://localhost:8082'),
-  ]).then(responses =>
-    res.render('index', { header: responses[0], productsList: responses[1], cart: responses[2] })
-  ).catch(error =>
-    res.send(error.message)
-  )
-);
+server.get('/', (req, res) => {
+  let appsToLoad = [
+    { name: 'header', url: 'http://localhost:8080' },
+    { name: 'productsList', url: 'http://localhost:8081' },
+    { name: 'cart', url: 'http://localhost:8082' }
+  ]
+  let previousFlushedData = '';
+
+  const load = (piece, index) =>
+    getContents(piece.url).then(data => {
+      appsToLoad[index].data = data;
+      flushData();
+    });
+
+  const buildDataForEjs = () =>
+    appsToLoad.reduce((params, app) => {
+      params[app.name] = app.data || 'PENDING';
+      return params;
+    }, {});
+
+  const flushData = () => {
+    const dataForEjs = buildDataForEjs();
+    ejs.renderFile('views/index.ejs', dataForEjs, {}, (err, renderedHtml) => {
+      if (err) throw err;
+
+      const htmlToBeFlushed = renderedHtml.split('PENDING')[0].replace(previousFlushedData, '');
+
+      // chunks have to have at least 4096 bytes to make sure browsers print them
+      res.write(padRight(htmlToBeFlushed, 4096, ' '));
+      previousFlushedData += htmlToBeFlushed;
+    });
+  }
+
+  flushData();
+  Promise.all(
+    appsToLoad.map(load)
+  ).then(() =>
+    res.end()
+  );
+});
 
 const getContents = (url) => new Promise((resolve, reject) => {
   request.get(url, (error, response, body) => {
     if (error) return resolve("Error loading " + url + ": " + error.message);
+
     return resolve(body);
   });
 });
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`Homage listening on port ${port}`);
+  console.log(`Homepage listening on port ${port}`);
 });
