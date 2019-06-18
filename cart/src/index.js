863aa584485d431aabb427fedbe285455ede630c
Bobby Lei <bobby.lei@hreasily.com>
2019-06-18 22:39:39 +1200

    Convert all microservices to render a webcomponent

--- cart/src/index.js
+++ cart/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Cart extends HTMLElement {
+  attachCallback() {
+    ReactDOM.render(<App />, this.createShadowRoot());
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+document.registerElement('microfrontends-cart', Cart);
