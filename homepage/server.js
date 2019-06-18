d79ab68d33a672b4d87db9ee2d011e6dc1a212e4
Bobby Lei <bobby.lei@hreasily.com>
2019-06-18 21:34:49 +1200

    Render using iframes

--- homepage/server.js
+++ homepage/server.js
@@ -4,24 +4,7 @@ const request = require('request');
 
 server.set('view engine', 'ejs');
 
-server.get('/', (req, res) =>
-  Promise.all([
-    getContents('http://localhost:8080'),
-    getContents('http://localhost:8081'),
-    getContents('http://localhost:8082'),
-  ]).then(responses =>
-    res.render('index', { header: responses[0], productsList: responses[1], cart: responses[2] })
-  ).catch(error =>
-    res.send(error.message)
-  )
-);
-
-const getContents = (url) => new Promise((resolve, reject) => {
-  request.get(url, (error, response, body) => {
-    if (error) return resolve("Error loading " + url + ": " + error.message);
-    return resolve(body);
-  });
-});
+server.get('/', (req, res) => res.render('index'));
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
